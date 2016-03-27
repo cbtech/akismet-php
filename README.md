2d9bf1b8439a542d49af4348a955eedd6419baae
Antonio Tajuelo <atajuelo@gmail.com>
2016-03-27 14:27:21 +0200

    Package upload

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # akismet-php
-Stop spam for hitting your website comments or content using Akismet.
+Stop spam from hitting your website comments or content using Akismet.
+
+## Getting Started
+
+First, get your Akismet key (if you still do not have one) from here: https://akismet.com/
+
+Then install the package using Composer and check the following code samples.
+
+## Code Sample
+
+```php
+$akismet = new Akismet('YOUR-AKISMET-KEY','YOUR-WEBSITE-URL');
+
+// Check if the Akismet key is valid
+$isValidKey = $akismet->verifyKey();
+var_dump($isValidKey);
+
+// Check if a comment is spam
+$params = [
+  'is_test'        => 1,
+  'user_ip'        => '123.123.123.123',
+  'user_agent'     => 'blablabla',
+  'comment_author' => 'viagra-test-123',
+  'coment_content' => 'spam'
+];
+$isSpamComment = $akismet->commentCheck($params);
+var_dump($isSpamComment);
+```
+
+## Available Methods
+
+- verifyKey
+- commentCheck
+- submitSpam
+- submitHam
+
+## Please View
+
+Akismet API Documentation: https://akismet.com/development/api/#detailed-docs
---
+++ src/Akismet.php
@@ -0,0 +1,61 @@
+<?php namespace AntonioTajuelo\Akismet;
+
+class Akismet
+{
+
+  private $key  = '';
+  private $blog = '';
+
+  function __construct($key = '',$blog = '')
+  {
+    $this->key  = $key;
+    $this->blog = $blog;
+  }
+
+  private function request($endpoint,$params = [])
+  {
+    $client = new \GuzzleHttp\Client(['http_errors' => false]);
+    $form_params = array_merge(['blog' => $this->blog],$params);
+    $request = $client->request(
+      'POST',
+      $endpoint,
+      [
+        'form_params' => $form_params
+      ]
+    );
+    return (string)$request->getBody();
+  }
+
+  public function verifyKey()
+  {
+    return $this->request(
+      'https://rest.akismet.com/1.1/verify-key',
+      ['key' => $this->key]
+      );
+  }
+
+  public function commentCheck($params)
+  {
+    return $this->request(
+      'https://' . $this->key . '.rest.akismet.com/1.1/comment-check',
+      $params
+      );
+  }
+
+  public function submitSpam($params)
+  {
+    return $this->request(
+      'https://' . $this->key . '.rest.akismet.com/1.1/submit-spam',
+      $params
+      );
+  }
+
+  public function commentCheck($params)
+  {
+    return $this->request(
+      'https://' . $this->key . '.rest.akismet.com/1.1/submit-ham',
+      $params
+      );
+  }
+
+}
